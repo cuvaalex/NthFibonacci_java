10bc33c97513a4d21ccf989f243778b5a8ff2d32
Alexandre Cuva <alexandre.cuva@gmail.com>
2020-12-29 09:50:11 +0100

    empty message

--- src/test/java/org/socraagile/kata/FibonacciShould.java
+++ src/test/java/org/socraagile/kata/FibonacciShould.java
@@ -2,14 +2,21 @@ package org.socraagile.kata;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FibonacciShould {
 
     @DisplayName("The fibonacci number of 1 is 1")
-    @Test
-    public void return_1_when_1() {
+    @ParameterizedTest
+    @CsvSource({
+        "0, 0",
+        "1, 1"
+    })
+    public void return_value_when_number(int number, int value) {
         Fibonacci fibonacci = new Fibonacci();
 
         int result = fibonacci.computeNth(1);
