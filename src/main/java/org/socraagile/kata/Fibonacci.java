0b73c53269dca25903909c701720d1d51dd9fdb6
Alexandre Cuva <alexandre.cuva@gmail.com>
2020-12-29 10:04:23 +0100

    empty message

--- src/main/java/org/socraagile/kata/Fibonacci.java
+++ src/main/java/org/socraagile/kata/Fibonacci.java
@@ -2,6 +2,8 @@ package org.socraagile.kata;
 
 public class Fibonacci {
     public int computeNth(int number) {
+        if( number > 1)
+            return 1;
         return number;
     }
 }
--- src/test/java/org/socraagile/kata/FibonacciShould.java
+++ src/test/java/org/socraagile/kata/FibonacciShould.java
@@ -14,7 +14,8 @@ public class FibonacciShould {
     @ParameterizedTest(name = "{index} => F({0}) => {1}")
     @CsvSource({
             "0, 0",
-            "1, 1"
+            "1, 1",
+            "2, 1"
     })
     public void return_value_when_number(int number, int value) {
         Fibonacci fibonacci = new Fibonacci();
